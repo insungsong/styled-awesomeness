9a2be0546a555c8cd58ec80720807805ba6bed45
INSUNG SONG <insungsong@INSUNGui-MacBook-Pro.local>
2020-04-25 15:54:12 +0900

    styled v4 see

--- src/App.js
+++ src/App.js
@@ -1,10 +1,71 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
+import styled, { createGlobalStyle, css, keyframes } from "styled-components";
 
-class App extends React.Component {
+const GlobalStyle = createGlobalStyle`
+  body{
+    padding:0;
+    margin:0;
+  }
+`;
+
+class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <React.Fragment>
+        <GlobalStyle />
+        <Container>
+          <Button>Hello</Button>
+          <Button danger rotationTime={5}>
+            Hello
+          </Button>
+          <Button>
+            <Acnchor href="http://google.com">go to google</Acnchor>
+          </Button>
+        </Container>
+      </React.Fragment>
+    );
   }
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${(props) => (props.danger ? "#e74c3c" : "#2ecc71")};
+  ${(props) => {
+    if (props.danger) {
+      return css`
+        animation: ${rotation} ${props.rotationTime}s linear infinite;
+      `;
+    }
+  }}
+`;
+
+const Acnchor = styled("a")`
+  text-decoration: none;
+`;
+
+const rotation = keyframes`
+  from{
+    transform:rotate(0deg);
+  }
+  to{
+    transform:rotate(360deg);
+  }
+`;
 
 export default App;
